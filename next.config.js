20f19830db78a831eb172adafa31d599695dc867
muhammadafiqyunanto27 <muhammadafiqyunanto@gmail.com>
2025-01-22 09:04:34 +0700

    add next and scripts

---
+++ next.config.js
@@ -0,0 +1,17 @@
+
+// next.config.js
+module.exports = {
+    async rewrites() {
+        // Hanya terapkan rewrites di development
+        return [
+            {
+                source: '/',
+                destination: '/index.html',
+            },
+            {
+                source: '/:path*',
+                destination: '/:path*',
+            },
+        ];
+    },
+ };
